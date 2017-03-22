2a623f638bc189e6c1af3156097ca69a249edda0
yasb001 <zhang-xch@qq.com>
2017-03-22 23:43:03 +0800

    ui create

---
+++ manual/mainwidget.cpp
@@ -0,0 +1,39 @@
+#include "mainwidget.h"
+#include "ui_mainwidget.h"
+
+MainWidget::MainWidget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::MainWidget)
+{
+    ui->setupUi(this);
+}
+
+MainWidget::~MainWidget()
+{
+    delete ui;
+}
+
+void MainWidget::on_pushButton_SignalEdit_clicked()
+{
+    if(ui->pushButton_SignalEdit->isChecked()){
+        ui->pushButton_SignalEdit->setText("保存修改");
+
+        ui->lineEdit_SignalFrom->setEnabled(true);
+        ui->lineEdit_SignalType->setEnabled(true);
+        ui->textEdit_SignalMeaning->setEnabled(true);
+        ui->textEdit_SignalConnected->setEnabled(true);
+        ui->textEdit_SignalReason->setEnabled(true);
+        ui->textEdit_SignalHandler->setEnabled(true);
+    }
+    else {
+        ui->pushButton_SignalEdit->setText("信号编辑");
+
+        ui->lineEdit_SignalFrom->setEnabled(false);
+        ui->lineEdit_SignalType->setEnabled(false);
+        ui->textEdit_SignalMeaning->setEnabled(false);
+        ui->textEdit_SignalConnected->setEnabled(false);
+        ui->textEdit_SignalReason->setEnabled(false);
+        ui->textEdit_SignalHandler->setEnabled(false);
+    }
+
+}
